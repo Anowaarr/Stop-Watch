551bd43af84967de94b053f451d97233b5f3e902
Anowaarr <itzanowar.jr@gmail.com>
2026-05-22 15:55:47 +0600

    Second commit

---
+++ script.js
@@ -0,0 +1,64 @@
+let time = document.getElementById("time");
+let clock = document.getElementById("clock");
+
+setInterval(() => {
+
+    let date = new Date();
+
+    let currentTime = date.toLocaleTimeString();
+
+    clock.innerHTML = currentTime;
+
+}, 1000);
+
+
+
+
+let Count = 0;
+
+let interval;
+
+function startWatch() {
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+
+        Count++;
+
+        time.innerHTML = Count;
+
+    }, 1000);
+
+}
+
+function stopWatch() {
+
+    clearInterval(interval);
+
+}
+
+
+let counters = Array.from(document.querySelectorAll(".counter"));
+
+counters.map((item) => {
+
+    let start = 0;
+
+    let endNumber = item.dataset.number;
+
+    let stopCounter = setInterval(() => {
+
+        start++;
+
+        item.innerHTML = start;
+
+        if (start == endNumber) {
+
+            clearInterval(stopCounter);
+
+        }
+
+    }, 20);
+
+});
